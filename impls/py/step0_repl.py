ded92222a29eba7918fa61d339769d5954844192
kawpuh <e.r.sorrell@gmail.com>
2022-09-02 19:31:59 -0500

    step 0

---
+++ impls/py/step0_repl.py
@@ -0,0 +1,27 @@
+import readline
+
+def READ(s):
+  return s
+
+def EVAL(s):
+  return s
+
+def PRINT(s):
+  print(s)
+
+def rep(s):
+  READ(EVAL(PRINT(s)))
+
+def main():
+  readline.read_init_file()
+  while(1):
+    try:
+      inp = input("user> ")
+    except EOFError:
+      print("\nExiting...")
+      return 0
+    readline.add_history(inp)
+    rep(inp)
+
+if __name__=="__main__":
+  main()
