6f64e9bcce256766b260ac0136a4ae0506d08f2e
kawpuh <e.r.sorrell@gmail.com>
2022-09-05 23:15:32 -0500

    step 4: if and do, fn* without varargs

--- impls/py/env.py
+++ impls/py/env.py
@@ -3,9 +3,14 @@ class UnboundSymbolError(BaseException):
 
 
 class Env:
-    def __init__(self, outer=None):
+    def __init__(self, outer={}, add_binds=None):
         self.outer = outer
         self.data = {}
+
+        if add_binds is not None:
+            key_list, val_list = add_binds
+            for (_, key_name), val in zip(key_list, val_list):
+                self.set(key_name, val)
 
     def set(self, k, v):
         self.data[k] = v
@@ -15,7 +20,7 @@ class Env:
         inner_val = self.data.get(k)
         if inner_val is not None:
             return inner_val
-        elif self.outer is not None:
+        elif self.outer.get(k) is not None:
             return self.outer.get(k)
         else:
             raise UnboundSymbolError
